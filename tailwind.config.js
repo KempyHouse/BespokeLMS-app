7e545ffdf6b3408151f0995adfb52eb0372088bd
Andrew <kemp.house@googlemail.com>
2026-07-23 07:51:49 +0100

    Fix Tailwind v4 configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './resources/views/**/*.blade.php',
     './resources/views/platform/**/*.blade.php',
     './resources/js/**/*.js',
   ],
-  theme: {
-    extend: {
-      colors: {
-        teachhq: 'var(--color-teachhq, #0084ff)',
-        slatecard: 'var(--color-slatecard, #1a202c)',
-        paper: 'var(--color-paper, #f5f5f5)',
-      },
-      borderRadius: {
-        control: 'var(--radius-control, 0.375rem)',
-      },
-    },
-  },
-  plugins: [],
 }
