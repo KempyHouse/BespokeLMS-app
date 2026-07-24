d14d564925af51f10a6b1a415558269244feafb2
Andrew <kemp.house@googlemail.com>
2026-07-24 02:01:21 +0100

    Remove My Certificates from MY WORKSPACE (duplicate of My Achievements)

--- resources/views/components/my-nav.blade.php
+++ resources/views/components/my-nav.blade.php
@@ -17,8 +17,6 @@
         'My workspace' => [
             ['key' => 'my-learning', 'label' => 'My Learning', 'href' => route('my.home'),
              'icon' => '<path d="M22 10 12 5 2 10l10 5 10-5Z"/><path d="M6 12v5c0 1 2 3 6 3s6-2 6-3v-5"/>'],
-            ['key' => 'my-certificates', 'label' => 'My Certificates', 'soon' => true,
-             'icon' => '<circle cx="12" cy="8" r="6"/><path d="M8.5 13.5 7 22l5-3 5 3-1.5-8.5"/>'],
             ['key' => 'my-achievements', 'label' => 'My Achievements', 'soon' => true,
              'icon' => '<path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6M18 9h1.5a2.5 2.5 0 0 0 0-5H18M4 22h16M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22M18 2H6v7a6 6 0 0 0 12 0V2Z"/>'],
         ],
